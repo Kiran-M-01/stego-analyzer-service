c91bfbe187db07a1a19f1a15b3a6ecd145153c37
TejasNayak-000 <mkiranmkiran306@gmail.com>
2026-07-19 02:49:51 +0530

    implemented analyze endpoint

--- routes/analyze.py
+++ routes/analyze.py
@@ -1,4 +1,8 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+
+from utils.validators import allowed_file
+from utils.image_loader import ImageLoader
+from analyzer.report import ReportAnalyzer
 
 analyze_bp = Blueprint("analyze", __name__)
 
@@ -9,6 +13,59 @@ def health():
         {
             "status": "healthy",
             "service": "Stego Analyzer Service",
-            "version": "1.0.0"
+            "version": "1.0.0",
         }
     )
+
+
+@analyze_bp.route("/analyze", methods=["POST"])
+def analyze():
+
+    if "image" not in request.files:
+        return jsonify(
+            {
+                "status": "error",
+                "message": "No image uploaded."
+            }
+        ), 400
+
+    file = request.files["image"]
+
+    if file.filename == "":
+        return jsonify(
+            {
+                "status": "error",
+                "message": "No file selected."
+            }
+        ), 400
+
+    if not allowed_file(file.filename):
+        return jsonify(
+            {
+                "status": "error",
+                "message": "Unsupported file format."
+            }
+        ), 400
+
+    try:
+
+        image, metadata = ImageLoader.load_image(file)
+
+        report = ReportAnalyzer.analyze(image)
+
+        return jsonify(
+            {
+                "status": "success",
+                "metadata": metadata,
+                "analysis": report,
+            }
+        )
+
+    except Exception as e:
+
+        return jsonify(
+            {
+                "status": "error",
+                "message": str(e),
+            }
+        ), 500
--- utils/image_loader.py
+++ utils/image_loader.py
@@ -5,11 +5,9 @@ import numpy as np
 class ImageLoader:
 
     @staticmethod
-    def load_image(image_path: str):
+    def load_image(source):
 
-        image = Image.open(image_path)
-
-        image = image.convert("RGB")
+        image = Image.open(source).convert("RGB")
 
         image_array = np.array(image)
 
@@ -17,8 +15,6 @@ class ImageLoader:
             "width": image.width,
             "height": image.height,
             "mode": image.mode,
-            "shape": image_array.shape,
-            "dtype": str(image_array.dtype)
         }
 
         return image_array, metadata
