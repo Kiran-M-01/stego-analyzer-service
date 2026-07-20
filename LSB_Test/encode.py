6ff5e93486e507bbaba2d78e60c7c2dc2f3e5ece
TejasNayak-000 <mkiranmkiran306@gmail.com>
2026-07-20 23:49:52 +0530

    Complete Image Analysis Microservice v1.0

--- LSB_Test/encode.py
+++ LSB_Test/encode.py
@@ -8,6 +8,6 @@ with open("message.txt", "r", encoding="utf-8") as file:
 secret_image = lsb.hide("test.png", secret_message)
 
 # Save the new image
-secret_image.save("stegoTest1_500words.png")
+secret_image.save("stegoTest.png")
 
 print("Stego image created successfully!")
